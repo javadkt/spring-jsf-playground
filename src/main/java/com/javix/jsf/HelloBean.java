4f95d9bada53eb9f4d9c0d85ebd8ca29280b4413
mohammedjavadkt <javadkt@gmail.com>
2023-09-30 11:11:09 +0400

    Dropdown

--- src/main/java/com/javix/jsf/HelloBean.java
+++ src/main/java/com/javix/jsf/HelloBean.java
@@ -1,21 +1,39 @@
 package com.javix.jsf;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 
 @ManagedBean
-@SessionScoped
+@RequestScoped
 public class HelloBean implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	
-	private String name;
+    private static final long serialVersionUID = 1L;
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+    private String name;
+
+    private String selectedDepartment;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSelectedDepartment() {
+        return selectedDepartment;
+    }
+
+    public void setSelectedDepartment(String selectedDepartment) {
+        this.selectedDepartment = selectedDepartment;
+    }
+
+    public void printSelectedDepartment() {
+        // You can access the selectedDepartment property here
+        System.out.println("Selected Department: " + selectedDepartment);
+    }
+
 }
